86037af6e20df5da7a1aa398a392ebe9240c3175
Marymuriga <marymuriga@gmail.com>
2022-07-30 23:41:49 +0300

    Updates

---
+++ precache-manifest.6ed4e6ba4f234cceee05c0aa62d2203d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af64add82d447b0cb84f",
+    "url": "/My-phase-2-project/static/css/main.e2fd0b34.chunk.css"
+  },
+  {
+    "revision": "af64add82d447b0cb84f",
+    "url": "/My-phase-2-project/static/js/main.af64add8.chunk.js"
+  },
+  {
+    "revision": "16bc04b3cc5d83859fc9",
+    "url": "/My-phase-2-project/static/js/1.16bc04b3.chunk.js"
+  },
+  {
+    "revision": "1dba0872b368b160533a",
+    "url": "/My-phase-2-project/static/js/runtime~main.1dba0872.js"
+  },
+  {
+    "revision": "2f60a59fa796d5dfaaa7604ef0bb4642",
+    "url": "/My-phase-2-project/index.html"
+  }
+];
